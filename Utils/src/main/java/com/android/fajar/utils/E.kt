45aa12f3554ed49997b45b9f91609287f6d53926
Fajar Anugrah Ramadhan <fajarconan@gmail.com>
2021-12-03 17:50:11 +0700

    update file
    update E

--- Utils/src/main/java/com/android/fajar/utils/E.kt
+++ Utils/src/main/java/com/android/fajar/utils/E.kt
@@ -1,13 +1,13 @@
 package com.android.fajar.utils
 
-open class E : EditTextValidator() {
+open class E {
 
-    open fun checkAll(var0: Array<EditTextSet>): Boolean {
+    open fun checkAll(var0: Array<EditTextValidator.EditTextSet>): Boolean {
         Helper.checkPermission()
         return checkAll(var0, true)
     }
 
-    open fun checkAll(var0: Array<EditTextSet>, var1: Boolean): Boolean {
+    private fun checkAll(var0: Array<EditTextValidator.EditTextSet>, var1: Boolean): Boolean {
         Helper.checkPermission()
         var var2 = true
         val var3 = var0
@@ -24,17 +24,17 @@ open class E : EditTextValidator() {
         return var2
     }
 
-    open fun checkAll(var0: List<EditTextSet?>): Boolean {
+    open fun checkAll(var0: List<EditTextValidator.EditTextSet?>): Boolean {
         Helper.checkPermission()
         return checkAll(var0, true)
     }
 
-    open fun checkAll(var0: List<EditTextSet?>, var1: Boolean): Boolean {
+    private fun checkAll(var0: List<EditTextValidator.EditTextSet?>, var1: Boolean): Boolean {
         Helper.checkPermission()
         var var2 = true
         val var3: Iterator<*> = var0.iterator()
         while (var3.hasNext()) {
-            val var4 = var3.next() as EditTextSet
+            val var4 = var3.next() as EditTextValidator.EditTextSet
             if (!var4.check()) {
                 var2 = false
                 if (var1) {
